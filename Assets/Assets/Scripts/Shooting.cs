164f36bb1eebc1b50809e4881448218e52707afa
Catabata <jtr0442@gmail.com>
2025-02-13 16:58:36 +0000

    Update Shooting.cs

--- Assets/Assets/Scripts/Shooting.cs
+++ Assets/Assets/Scripts/Shooting.cs
@@ -11,20 +11,21 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame      
     void Update()                           
     {
-
-
-    }
-    private void FixedUpdate()
-    {
-        LayerMask main = LayerMask.GetMask("Default");
-        Vector3 mousepos = Input.mousePosition;
-        Vector2 mouseDir = (Vector2)(mousepos - transform.position).normalized;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, mouseDir, main);
-        if (Input.GetKeyDown(mouse 1))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
 
             print("HELLLLLLPPP");
         }
+
+    }
+    private void FixedUpdate()
+    {
+
+        LayerMask main = LayerMask.GetMask("Default");
+        Vector3 mousepos = Input.mousePosition;
+        Vector2 mouseDir = (Vector2)(mousepos - transform.position).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, mouseDir, main);
+        
 
     }
 
